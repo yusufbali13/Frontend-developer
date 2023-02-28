47fbc2d050a22017a2fa77e5c988a0ece8e65ce9
Yusuf <baliyusuf675@gmail.com>
2023-02-28 17:25:00 +0300

    w1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import './App.css';
-import Footer from './components/footer/Footer';
-import Header from './components/header/Header';
-import Main from './components/main/Main';
-import data from "./data"
+import "./App.css";
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+import Main from "./components/main/Main";
+import data from "./data";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      {data.map((item)=> <Main {...item}/>)}
-      <Footer/>      
+      <Header />
+      {data.map((item) => (
+        <Main {...item} />
+      ))}
+      <Footer />
     </div>
   );
 }
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,24 +1,24 @@
-import "./Nav.css"
+import "./Nav.css";
 
 const Nav = () => {
   return (
     <div className="nav">
-        <ul>
-            <li>
-                <a href="#">Home</a>
-            </li>
-            <li>
-                <a href="#">My Skils</a>
-            </li>
-            <li>
-                <a href="#">My Projects</a>
-            </li>
-            <li>
-                <a href="#">Contact</a>
-            </li>
-        </ul>
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">My Skils</a>
+        </li>
+        <li>
+          <a href="#">My Projects</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
