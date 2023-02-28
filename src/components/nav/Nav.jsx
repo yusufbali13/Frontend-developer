ef76f644f6fdf8eaea4e81ac10c60c09b95d4855
Yusuf <baliyusuf675@gmail.com>
2023-02-28 17:56:36 +0300

    nav

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -5,16 +5,16 @@ const Nav = () => {
     <div className="nav">
       <ul>
         <li>
-          <a href="#">Home</a>
+          <a href="nav">Home</a>
         </li>
         <li>
-          <a href="#">My Skils</a>
+          <a href="nav">My Skils</a>
         </li>
         <li>
-          <a href="#">My Projects</a>
+          <a href="nav">My Projects</a>
         </li>
         <li>
-          <a href="#">Contact</a>
+          <a href="nav">Contact</a>
         </li>
       </ul>
     </div>
